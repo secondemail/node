64b7a575fd8b7733087bcff1fb89ea590af973c0
secondemail <166629198+secondemail@users.noreply.github.com>
2024-04-18 15:53:36 +0200

    Update orderService.js

--- controllers/orderService.js
+++ controllers/orderService.js
@@ -208,7 +208,7 @@ exports.webhookCheckout = (req, res, next) => {
   let event;
   try {
     event = stripe.webhooks.constructEvent(
-      req.rawBody,
+      req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
     );
